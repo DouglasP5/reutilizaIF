38cbbeb2391a9a8ed8b260e4c099d8938a939c6e
dev-willem <willembergfilho@gmail.com>
2026-06-11 14:54:06 -0300

    chore: refact project mvd

--- config.py
+++ config.py
@@ -1,17 +1,12 @@
 import os
-from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
+
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    
-    # Usar SQLite para desenvolvimento local
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///reutilizaif.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///reutilizaif.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    SUAP_API_BASE_URL = "https://suap.ifrn.edu.br"
+    SUAP_API_BASE_URL = 'https://suap.ifrn.edu.br'
     ADMIN_MATRICULAS = {'20231041110013'}
-
